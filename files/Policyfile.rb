f9ea7c22d72648c525fbe7d23ee118d18b58204a
devoptimist <sbrown@chef.io>
2019-06-14 07:38:12 +0200

    latest cookbook for chef server

--- files/Policyfile.rb
+++ files/Policyfile.rb
@@ -4,5 +4,5 @@ default_source :supermarket
 run_list 'chef_server_wrapper::default'
 named_run_list :chef_server, 'chef_server_wrapper::default'
 
-cookbook 'chef_server_wrapper', github: 'devoptimist/chef_server_wrapper', tag: 'v0.1.10'
+cookbook 'chef_server_wrapper', github: 'devoptimist/chef_server_wrapper', tag: 'v0.1.11'
 cookbook 'chef-ingredient'    , github: 'chef-cookbooks/chef-ingredient', tag: 'v3.1.1'
