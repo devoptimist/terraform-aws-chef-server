7c6afa2370bb8c85a28649d543704cf3a1f60b11
devoptimist <sbrown@chef.io>
2019-06-16 23:26:18 +0100

    updating output

--- outputs.tf
+++ outputs.tf
@@ -19,7 +19,7 @@ output "validation_pem" {
 }
 
 output "validation_client_name" {
-  value = data.external.chef_server_details.result["validation_client_name"]
+  value = "${data.external.chef_server_details.result["validation_client_name"]}.pem"
 }
 
 output "client_pem" {
